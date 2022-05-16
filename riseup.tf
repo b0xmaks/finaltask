03e3207f1d26e68f5757b6fb8ee6032a7dcc4b93
b0xmaks <m.3anin@gmail.com>
2022-05-16 14:33:09 +0300

    main

--- riseup.tf
+++ riseup.tf
@@ -103,7 +103,7 @@ resource "yandex_vpc_network" "network-1" {
 //  zone           = "ru-central1-b"
 //  network_id     = yandex_vpc_network.network-1.id
 //  v4_cidr_blocks = ["192.168.10.0/24"]
-}
+//}
 
 //output "internal_ip_address_vm_1" {
 //  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
