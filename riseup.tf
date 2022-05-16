068d5baeb6849eb9f16ef1d34078fc99f6145396
b0xmaks <m.3anin@gmail.com>
2022-05-16 13:49:57 +0300

    main

--- riseup.tf
+++ riseup.tf
@@ -53,10 +53,10 @@ resource "yandex_compute_instance" "vm-1" {
     }
   }
 
-network_interface {
-  subnet_id = yandex_vpc_subnet.subnet-1.id
-  nat       = true
-}
+//network_interface {
+//  subnet_id = yandex_vpc_subnet.subnet-1.id
+//  nat       = true
+//}
 
   metadata = {
     ssh-keys = "ubuntu:${file("/keys/id_rsa.pub")}"
@@ -83,40 +83,40 @@ resource "yandex_compute_instance" "vm-2" {
     }
   }
 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
+//  network_interface {
+//    subnet_id = yandex_vpc_subnet.subnet-1.id
+//    nat       = true
+//  }
 
   metadata = {
     ssh-keys = "ubuntu:${file("/keys/id_rsa.pub")}"
   }
 }
 
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
+//resource "yandex_vpc_network" "network-1" {
+//  name = "network1"
+//}
 
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
+//resource "yandex_vpc_subnet" "subnet-1" {
+//  name           = "subnet1"
+//  zone           = "ru-central1-b"
+//  network_id     = yandex_vpc_network.network-1.id
+//  v4_cidr_blocks = ["192.168.10.0/24"]
+//}
 
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
+//output "internal_ip_address_vm_1" {
+//  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+//}
 
-output "internal_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
-}
+//output "internal_ip_address_vm_2" {
+//  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+//}
 
 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
+//output "external_ip_address_vm_1" {
+//  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+//}
 
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
-}
+//output "external_ip_address_vm_2" {
+//  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+//}
