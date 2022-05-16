44ac1ae6d40924f90b199ac9b30f8404224316f7
b0xmaks <m.3anin@gmail.com>
2022-05-16 14:45:49 +0300

    main

--- riseup.tf
+++ riseup.tf
@@ -94,16 +94,16 @@ resource "yandex_compute_instance" "vm-2" {
 }
 
 resource "yandex_vpc_network" "network-1" {
-//  name = "network1"
-  name = "default"
+  name = "network1"
+//  name = "default"
 }
 
-//resource "yandex_vpc_subnet" "subnet-1" {
-//  name           = "subnet1"
-//  zone           = "ru-central1-b"
-//  network_id     = yandex_vpc_network.network-1.id
-//  v4_cidr_blocks = ["192.168.10.0/24"]
-//}
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "default"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["10.129.0.0/24"]
+}
 
 //output "internal_ip_address_vm_1" {
 //  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
