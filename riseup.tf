45ae238d80510fd2f50a7472f0ca2d6bb009d907
b0xmaks <m.3anin@gmail.com>
2022-05-17 15:46:36 +0300

    main

--- riseup.tf
+++ riseup.tf
@@ -24,7 +24,7 @@ provider "yandex" {
 
 resource "null_resource" "hosts" {
   provisioner "local-exec" {
-    command = ["sudo su", "chmod 0777 /etc/ansible/hosts"]
+    command = "sudo su", "chmod 0777 /etc/ansible/hosts"
   }
 }
 
