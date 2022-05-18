5a8599708f5d672dc530ad61f4965c31af9f3e51
b0xmaks <m.3anin@gmail.com>
2022-05-19 01:31:32 +0300

    main

--- riseup.tf
+++ riseup.tf
@@ -109,7 +109,7 @@ ${yandex_compute_instance.vm-1.network_interface.0.nat_ip_address}
 ansible_connection=ssh
 ansible_user=ubuntu
 ansible_ssh_private_key_file=/keys/id_rsa
-ansible_docker_extra_args='-o StrictHostKeyChecking=no'
+ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 
 
 [stage]
@@ -119,7 +119,7 @@ ${yandex_compute_instance.vm-2.network_interface.0.nat_ip_address}
 ansible_connection=ssh
 ansible_user=ubuntu
 ansible_ssh_private_key_file=/keys/id_rsa
-ansible_docker_extra_args="'-o StrictHostKeyChecking=no'"
+ansible_ssh_common_args='-o StrictHostKeyChecking=no'
     EOT
 }
 
